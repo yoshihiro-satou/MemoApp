246f8fa35de8437bb236956569e87f2de232be98
yoshihiro satou <128808496+yoshihiro-satou@users.noreply.github.com>
2025-10-08 20:36:31 +0900

    headerLayout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,15 +4,17 @@ import { View, Text, StyleSheet } from 'react-native'
 const Index = (): JSX.Element => {
   return (
     <View style={styles.container}>
-      <View>
-        <View>
-          <Text>Memo App</Text>
-          <Text>ログアウト</Text>
+
+      <View style={styles.header}>
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>Memo App</Text>
+          <Text style={styles.headerRight}>ログアウト</Text>
         </View>
       </View>
       <View>
 
         <View>
+
           <View>
             <Text>買い物リスト</Text>
             <Text>2013年10月1日 10:00</Text>
@@ -54,8 +56,28 @@ const Index = (): JSX.Element => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#ffffff'
+  },
+  header: {
+    backgroundColor: '#467FD3',
+    height: 104,
+    justifyContent: 'flex-end',
+  },
+  headerInner: {
     alignItems: 'center'
+  },
+  headerRight: {
+    position: 'absolute',
+    right: 16,
+    bottom: 16,
+    color: 'rgba(255, 255, 255, 0.7)'
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    color: '#ffffff'
   }
 })
 export default Index;
